925b79a53a654feeb6c9de536501c652dc2d5914
Hugo Josefson <hugo@josefson.org>
2015-11-30 21:41:27 +0100

    11

---
+++ 11.js
@@ -0,0 +1,4 @@
+export default (Bacon, nidelva, leirelva, buttonClicked) =>
+    nidelva
+        .combine(leirelva, (leirelvaSample, nidelvaSample) => leirelvaSample + nidelvaSample)
+        .sampledBy(buttonClicked)
