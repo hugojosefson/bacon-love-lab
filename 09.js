d1fbbb732beaf3f3a70d187e30abddf129aaa2be
Hugo Josefson <hugo@josefson.org>
2015-11-30 20:36:12 +0100

    09

---
+++ 09.js
@@ -0,0 +1,8 @@
+export default (Bacon, sector1Count, sector2Count, sector3Count, sector4Count) =>
+    Bacon.combineTemplate({
+        sector1: sector1Count,
+        sector2: sector2Count,
+        sector3: sector3Count,
+        sector4: sector4Count,
+        sector5: Bacon.constant(0)
+    })
