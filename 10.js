e9c0bb6c5de28a439fd85e887bbae51de6ecbe31
Hugo Josefson <hugo@josefson.org>
2015-11-30 21:19:37 +0100

    10

---
+++ 10.js
@@ -0,0 +1,12 @@
+export default (Bacon, riverFlow, inCriticalMode, isOnBreak, isSingleGate, systemActive, riverFlowLimit) => {
+    const isTooMuchWater = riverFlow
+        .map(flow => flow > riverFlowLimit)
+        .toProperty();
+
+    const isAllowedToNotify = isOnBreak.not()
+        .and(
+            inCriticalMode.or(systemActive.and(isSingleGate))
+        );
+
+    return isTooMuchWater.and(isAllowedToNotify);
+}
