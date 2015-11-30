b6044dfb60531164ad46b91d377feacaaf7200f0
Hugo Josefson <hugo@josefson.org>
2015-11-30 22:50:29 +0100

    15

---
+++ 15.js
@@ -0,0 +1,6 @@
+export default (Bacon, clicks, startAsyncTask) => {
+    const request = clicks.map(true);
+    const response = request.flatMap(startAsyncTask);
+
+    return request.awaiting(response);
+}
