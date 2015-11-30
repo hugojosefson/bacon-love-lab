376aa519bf7bf82e032372c805fba4330787b05d
Hugo Josefson <hugo@josefson.org>
2015-11-30 20:15:03 +0100

    07

---
+++ 07.js
@@ -0,0 +1,22 @@
+export default (Bacon, enteringShips, destroyerPosition) => {
+    const shipTally = enteringShips
+        .filter(ship => ship.type === 'zrrk')
+        .map(1)
+        .scan(0, (acc, v) => acc + v);
+
+    const destroyerHasEntered = destroyerPosition.map(distance => distance < 1);
+
+    const threatReport = enteringShips
+        .filter(destroyerHasEntered)
+        .take(5)
+        .fold({}, (report, ship) => {
+            if (!report[ship.type]) {
+                report[ship.type] = 0;
+            } else {
+                report[ship.type] += 1;
+            }
+            return report;
+        });
+
+    return {shipTally, threatReport};
+}
