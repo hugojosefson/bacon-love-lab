5d57effc18eb83cf8bfc6ddb64d51cab6deffe15
Hugo Josefson <hugo@josefson.org>
2015-11-30 19:52:57 +0100

    06

---
+++ 06.js
@@ -0,0 +1,18 @@
+export default (Bacon, enteringShips, destroyerPosition) => {
+    const ships = enteringShips.map(ship => ship.type === 'zrrk' ? 1 : 0);
+    const threat = destroyerPosition.map(distance => {
+        if (distance > 5) {
+            return 'low';
+        } else if (distance > 2) {
+            return 'medium';
+        } else if (distance > 1) {
+            return 'high';
+        } else {
+            return 'extreme';
+        }
+    });
+    const extremeThreat = threat.filter(threat => threat === 'extreme');
+    const postArrivalShips = ships.filter(extremeThreat);
+
+    return {ships, threat, postArrivalShips};
+}
