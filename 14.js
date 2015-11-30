7b0e19c98f74e019c6596c7ca4428ab02858397e
Hugo Josefson <hugo@josefson.org>
2015-11-30 22:44:59 +0100

    14

---
+++ 14.js
@@ -0,0 +1,14 @@
+export default (Bacon, fieldA, validationA, fieldB, validationB, fieldC, validationC) => {
+    const a = fieldA.map(validationA).toProperty(false);
+    const b = fieldB.map(value => value ? validationB(value) : true).toProperty(true);
+    const c = fieldC.map(validationC).toProperty(false);
+
+    const form = a.and(b).and(c);
+
+    return {
+        fieldAValid: a,
+        fieldBValid: b,
+        fieldCValid: c,
+        formValid: form
+    };
+}
